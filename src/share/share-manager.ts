fa677aa8c429a0dca8ef983986456bdaf423b4eb
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2024-07-23 09:16:22 +0200

    update faction share scripts

--- src/share/share-manager.ts
+++ src/share/share-manager.ts
@@ -1,78 +1,93 @@
 import { NS } from "@ns";
+import { run } from "/system/proc/run";
 
 export function autocomplete(data: any, args: any) {
     return [...data.servers];
 }
 
-function getMultiplier(threads: number) {
+function getMultiplier(threads: number): number {
     const mult = 1 + Math.log(threads) / 25;
     if (isNaN(mult) || !isFinite(mult)) return 1;
     return mult;
+}
+
+function getThreads(multiplier: number): number {
+    return Math.exp((multiplier - 1) * 25);
 }
 
 /**
  * @param {NS} ns
  */
 export async function main(ns: NS) {
-    const servers = new Set(ns.args as string[]);
+    const workerCost = ns.getScriptRam("share/share.js");
 
-    for (const server of servers) {
-        if (!ns.serverExists(server)) {
-            servers.delete(server);
-            ns.tprint(
-                `WARNING: No server called \x1b[1m${server}\x1b[0m. Ignoring.`,
-            );
-            continue;
+    function byThreads(threads: number): number[] | null {
+        const pids = [];
+
+        const reservations = globalThis.system.memory.reserveTotal(
+            threads * workerCost,
+        );
+        if (!reservations) return null;
+
+        for (const reservation of reservations) {
+            const size = globalThis.system.memory.sizeOf(reservation);
+            if (!size) continue;
+            const threads = Math.floor(size / workerCost);
+            const [pid] = run(ns, "share/share.js", {
+                threads,
+                temporary: true,
+                useReservation: reservation,
+            });
+            if (pid <= 0) {
+                for (const pid of pids) ns.kill(pid);
+                return null;
+            }
+
+            pids.push(pid);
         }
 
-        if (!ns.hasRootAccess(server)) {
-            servers.delete(server);
-            ns.tprint(
-                `WARNING: \x1b[1m${server}\x1b[0m exists, but we don't have root access, so we can't run scripts on it. Ignoring.`,
-            );
-        }
+        return pids;
     }
 
-    if (servers.size === 0) {
-        ns.tprint(`ERROR: No valid servers specified. Aborting.`);
+    function byMultiplier(targetMult: number): number[] | null {
+        const threads = getThreads(targetMult);
+        return byThreads(threads);
+    }
+
+    const { threads, multiplier } = ns.flags([
+        ["threads", 0],
+        ["multiplier", 0],
+    ]) as { threads: number; multiplier: number };
+
+    if (threads > 0 && multiplier > 0) {
+        ns.tprint("ERROR: only specify one of threads or multiplier.");
         return;
     }
 
     const pids: number[] = [];
+    if (threads > 0) {
+        const started = byThreads(threads);
+        if (!started)
+            return ns.tprint(
+                `ERROR: failed to reserve ${threads} sharing threads.`,
+            );
+        pids.push(...started);
+    } else if (multiplier > 0) {
+        if (multiplier <= 1)
+            return ns.tprint(`ERROR: multiplier needs to be >= 1.`);
+        const started = byMultiplier(multiplier);
+        if (!started)
+            return ns.tprint(
+                `ERROR: failed to reserve enough threads for a share multiplier of ${multiplier} (${getThreads(multiplier)}t)`,
+            );
+        pids.push(...started);
+    }
+
     ns.atExit(() => {
         for (const pid of pids) {
             ns.kill(pid);
         }
     });
-
-    for (const server of servers) {
-        ns.scp("share.js", server);
-    }
-
-    let totalThreads = 0;
-
-    const workerCost = ns.getScriptRam("share.js");
-
-    for (const server of servers) {
-        const usableRam =
-            ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
-        const threads = Math.floor(usableRam / workerCost);
-
-        const pid = ns.exec("share.js", server, { threads, temporary: true });
-        if (pid === 0) {
-            ns.tprint(`ERROR: Failed to launch share.js on ${server}.`);
-            continue;
-        }
-
-        pids.push(pid);
-        totalThreads += threads;
-    }
-
-    const bonusGain = getMultiplier(totalThreads) - 1;
-
-    ns.tprint(
-        `Running \x1b[36;1m${totalThreads}\x1b[0m (\x1b[36m${ns.formatRam(totalThreads * workerCost)}\x1b[0m) share threads across \x1b[1m${servers.size}\x1b[0m servers for a rep gain bonus of \x1b[36;1m${ns.formatPercent(bonusGain)}\x1b[0m.`,
-    );
 
     while (true) {
         await ns.sleep(1000);
--- src/share/share.ts
+++ src/share/share.ts
@@ -1,6 +1,10 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS) {
+    ns.atExit(() => {
+        globalThis.system.proc.killed(ns);
+    });
+
     while (true) {
         await ns.share();
     }
