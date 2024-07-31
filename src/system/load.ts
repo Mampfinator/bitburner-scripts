066845d659d9e5c8cfc4412b20ab3892e699dd69
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2024-07-31 17:22:41 +0200

    add configurable startup time compression factor

--- src/system/load.ts
+++ src/system/load.ts
@@ -14,14 +14,27 @@ import { load as loadTime } from "./compress-time";
 import { load as loadDependencies } from "./dependencies";
 import { compressTime } from "./compress-time";
 import { ServerCache } from "/lib/servers/server-cache";
+import { JSONSettings } from "/lib/settings";
+
+class CoreSettings extends JSONSettings {
+    constructor(ns: NS) {
+        super(ns, "/settings/core.json");
+    }
+
+    public timeCompressionFactor: number = -1;
+}
 
 export async function load(ns: NS) {
+    const settings = new CoreSettings(ns);
+    settings.load();
+    settings.save();
+
     await loadDependencies(ns);
 
     console.log("Loading system namespace...");
 
     await loadTime(ns);
-    compressTime(-1);
+    compressTime(settings.timeCompressionFactor);
 
     const system = {} as (typeof globalThis)["system"];
 
