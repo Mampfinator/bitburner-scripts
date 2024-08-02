3d7b3cc44bf559316b2dba1bfe06eee97982205d
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2024-08-02 16:09:17 +0200

    run formatter

--- src/cleanup.ts
+++ src/cleanup.ts
@@ -19,39 +19,50 @@ function getServerNames(ns: NS, startFrom: string = "home") {
     return [...hostnames];
 }
 
-const sleep = (ms: number) => new Promise(resolve => {
-    (globalThis.originalSetTimeout ?? globalThis.setTimeout)(resolve, ms);
-})
+const sleep = (ms: number) =>
+    new Promise((resolve) => {
+        (globalThis.originalSetTimeout ?? globalThis.setTimeout)(resolve, ms);
+    });
 
 async function connectToRemoteApi(): Promise<boolean> {
     const doc = eval("document") as Document;
 
-    const settingsButton: HTMLDivElement | null = doc.querySelector("div[role='button']:has(svg[aria-label='Options'])");
-    
-    console.log(settingsButton)
+    const settingsButton: HTMLDivElement | null = doc.querySelector(
+        "div[role='button']:has(svg[aria-label='Options'])",
+    );
+
+    console.log(settingsButton);
     if (!settingsButton) return false;
 
     settingsButton.click();
 
     await sleep(500);
 
-    const settingsPage = doc.querySelector("div.MuiContainer-root.MuiContainer-maxWidthLg.MuiContainer-disableGutters.css-5sgl1v");
+    const settingsPage = doc.querySelector(
+        "div.MuiContainer-root.MuiContainer-maxWidthLg.MuiContainer-disableGutters.css-5sgl1v",
+    );
     console.log(settingsPage);
     if (!settingsPage) return false;
 
-    const settingsTabs = settingsPage.querySelectorAll("div.MuiButtonBase-root.MuiListItemButton-root.MuiListItemButton-gutters.MuiListItemButton-root.MuiListItemButton-gutters.css-1593q0[role='button']");
+    const settingsTabs = settingsPage.querySelectorAll(
+        "div.MuiButtonBase-root.MuiListItemButton-root.MuiListItemButton-gutters.MuiListItemButton-root.MuiListItemButton-gutters.css-1593q0[role='button']",
+    );
     console.log(settingsTabs);
     if (!settingsTabs) return false;
-    
+
     const remoteApi = settingsTabs.item(settingsTabs.length - 1)! as HTMLDivElement;
     console.log(remoteApi);
     remoteApi.click();
     await sleep(500);
 
-    const remoteApiMenu: HTMLDivElement | null = settingsPage.querySelector("div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.css-d7p367");
+    const remoteApiMenu: HTMLDivElement | null = settingsPage.querySelector(
+        "div.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.css-d7p367",
+    );
     if (!remoteApiMenu) return false;
 
-    const connectButton: HTMLButtonElement | null = remoteApiMenu.querySelector("button.MuiButtonBase-root.MuiButton-root.MuiButton-text.css-gew9km[type='button']");
+    const connectButton: HTMLButtonElement | null = remoteApiMenu.querySelector(
+        "button.MuiButtonBase-root.MuiButton-root.MuiButton-text.css-gew9km[type='button']",
+    );
     if (!connectButton) return false;
 
     connectButton.click();
@@ -66,7 +77,7 @@ async function connectToRemoteApi(): Promise<boolean> {
 declare global {
     var appSaveFns: {
         triggerSave: () => Promise<void>;
-    }
+    };
 }
 
 export async function main(ns: NS) {
@@ -84,20 +95,22 @@ export async function main(ns: NS) {
         ns.tprint(`Deleting all files on ${server}`);
         for (const file of ns.ls(server, ".js")) {
             if (file.endsWith(".json") && !file.endsWith("dependencies.json")) continue;
-            ns.rm(file, server); 
+            ns.rm(file, server);
         }
     }
 
     const success = await connectToRemoteApi();
     if (!success) {
-        ns.alert("All files were deleted, but connection to the remote API for reacquiring files failed. Please connect manually, save and reload the game.");
+        ns.alert(
+            "All files were deleted, but connection to the remote API for reacquiring files failed. Please connect manually, save and reload the game.",
+        );
     }
 
     // wait for files to be transferred
     await sleep(2500);
 
     await globalThis.appSaveFns.triggerSave();
-    
+
     // I don't trust triggerSave. A second should be enough to save.
     await sleep(1000);
     globalThis.location.reload();
