ae23e38cdf6c2dc556bf6fb3ca80df8ad514595f
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2024-08-07 20:11:37 +0200

    refactor `getServer(Name)s` functions

--- src/lib/servers/names.ts
+++ src/lib/servers/names.ts
@@ -1,15 +1,11 @@
 import { NS } from "@ns";
 
-export function getServerNames(ns: NS, startFrom: string = "home") {
+export function getServerNames(ns: NS, startFrom = "home") {
     const hostnames = new Set([startFrom]);
-    const queue = [startFrom];
 
-    while (queue.length > 0) {
-        const current = queue.shift();
-
-        for (const hostname of ns.scan(current).filter((name) => !hostnames.has(name))) {
-            hostnames.add(hostname);
-            queue.push(hostname);
+    for (const hostname of hostnames) {
+        for (const neighbor of ns.scan(hostname)) {
+            hostnames.add(neighbor);
         }
     }
 
--- src/lib/servers/servers.ts
+++ src/lib/servers/servers.ts
@@ -1,15 +1,6 @@
-import { NS } from "@ns";
+import { NS, Server } from "@ns";
 import { getServerNames } from "./names";
 
-/**
- * Lists all servers reachable by scanning, starting from `startFrom`.
- *
- * @param {NS} ns
- * @param {string} startFrom
- *
- * @returns {Server[]}
- */
-export function getServers(ns: NS, startFrom: string = "home") {
-    const hostnames = getServerNames(ns, startFrom);
-    return [...hostnames].map((name) => ns.getServer(name));
+export function getServers(ns: NS, startFrom = "home"): Server[] {
+    return [...getServerNames(ns, startFrom)].map((h) => ns.getServer(h));
 }
